d9ff476d818fac197c595bb157a6a78f1de4ce8f
rustemersoyleyen <rustemersoyleyen@gmail.com>
2026-07-30 19:19:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
  // base: process.env.GITHUB_PAGES ? "/ingilizce-tarifler/" : "/",
-       base: '/blog/ingilizce-tarifler/',
+   base: process.env.CF_PAGES
+    ? '/blog/ingilizce-tarifler/'
+    : process.env.GITHUB_PAGES
+      ? '/ingilizce-tarifler/'
+      : '/',
 });
